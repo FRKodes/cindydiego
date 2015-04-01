5c41998b7f1fe8266833d60e70db630d6d00d010
frkodes <frkalderon@gmail.com>
2015-04-01 17:00:31 -0600

    styles changed

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,7 +1,3 @@
 <link href='http://fonts.googleapis.com/css?family=Pinyon+Script' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Source+Serif+Pro:400,700' rel='stylesheet' type='text/css'>
-@if($_ENV['APP_ENV'] == 'production')
-	<link rel="stylesheet" href="{{ elixir('css/all.min.css')}}">
-@else
-	<link rel="stylesheet" href="{{ elixir('css/all.min.css')}}">
-@endif
+<link rel="stylesheet" href="{{ elixir('css/all.min.css')}}">
