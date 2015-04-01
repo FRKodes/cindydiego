1c9555c9a5602c235ec7de212537b406c14bdc85
frkodes <frkalderon@gmail.com>
2015-04-01 17:30:59 -0600

    Fixing fonts

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,3 +1,3 @@
-<link href='http://fonts.googleapis.com/css?family=Pinyon+Script' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Source+Serif+Pro:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Pinyon+Script' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Source+Serif+Pro:400,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="{{ elixir('css/all.min.css')}}">
