3d8107bbe684b03eff581414338f8b68b66dfbfd
frkodes <frkalderon@gmail.com>
2015-04-06 15:57:36 -0500

    Adding some app keys

--- config/services.php
+++ config/services.php
@@ -14,24 +14,8 @@ return [
 	|
 	*/
 
-	'mailgun' => [
-		'domain' => '',
-		'secret' => '',
-	],
-
 	'mandrill' => [
-		'secret' => '',
-	],
-
-	'ses' => [
-		'key' => '',
-		'secret' => '',
-		'region' => 'us-east-1',
-	],
-
-	'stripe' => [
-		'model'  => 'App\User',
-		'secret' => '',
-	],
+		'secret' => $_ENV['MANDRILL_SECRET'],
+	]
 
 ];
