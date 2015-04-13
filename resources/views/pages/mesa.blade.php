ae09e5417a87a5c965fced0667a6882d86068ce1
frkodes <frkalderon@gmail.com>
2015-04-13 12:08:55 -0500

    Adding links to liverpool and Palacio de Hierro

--- resources/views/pages/mesa.blade.php
+++ resources/views/pages/mesa.blade.php
@@ -10,8 +10,8 @@
 
 		<p class="bold centered phrase">PRÓXIMAMENTE</p>
 
-		<p class="centered">LIVERPOOL</p>
-		<p class="centered">Palacio de Hierro</p>
+		<p class="centered"><a href="http://www.liverpool.com.mx/tienda/giftregistry/giftRegistryDetail.jsp?eventNo=1&eventDNo=34835649&_requestid=1762911#" target="_blank" title="Ver mesa de regalos en LIVERPOOL">LIVERPOOL</a></p>
+		<p class="centered"><a href="http://www.elpalaciodehierro.com/giftregistry/view/index/id/282474/#results" target="_blank" title="Ver mesa de regalos en El Palacio de Hierro">Palacio de Hierro</a></p>
 		<p class="centered"><a href="http://www.miregalopara.com" target="_blank" title="Mi regalo para...">www.miregalopara.com</a></p>
 
 		<div class="lg-timer" id="lg-timer"></div>
